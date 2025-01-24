6bedc66c0840211d6e623dde538f92b2fa6d6e53
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-01-25 00:31:04 +0100

    feat: add apiConfig for centralized api access

---
+++ src/config/apiConfig.js
@@ -0,0 +1,9 @@
+const API_BASE_URL = "http://localhost:3000/api/v1";
+
+const API_PATHS = {
+  SIGNUP: `${API_BASE_URL}/users/signup`,
+  LOGIN: `${API_BASE_URL}users/login`,
+  // Add more endpoints here as needed
+};
+
+export default API_PATHS;
