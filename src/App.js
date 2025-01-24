31b2149a87d6614ac74f28c920f54ebbfa8e959f
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-01-24 23:36:38 +0100

    feat: add loader on app start (#14)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { View, Image, StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AppNavigator from "./navigation/AppNavigator";
 
-const App = () => (
-  <GestureHandlerRootView style={{ flex: 1 }}>
-    <AppNavigator />
-  </GestureHandlerRootView>
-);
+import LogoSmallText from "./components/logo/LogoSmallText";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loadResources = () => {
+      // Load resources here
+      console.log("No resources to load yet");
+    };
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+
+    loadResources();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.loaderContainer}>
+        <LogoSmallText width={239} height={176} />
+      </View>
+    );
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppNavigator />
+    </GestureHandlerRootView>
+  );
+};
+
+const styles = StyleSheet.create({
+  loaderContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+});
 
 export default App;
