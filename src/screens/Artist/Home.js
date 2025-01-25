3d6ff03f1a5804ed302fd5ea4115a8af47b4a43f
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-01-25 18:17:17 +0100

    chore: display userId from async-storage on homepage

--- src/screens/Artist/Home.js
+++ src/screens/Artist/Home.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Home = ({ navigation }) => (
-  <View>
-    <Text>Artist Home</Text>
-    <Button
-      title="Go to Details"
-      onPress={() => navigation.navigate("Details")}
-    />
-  </View>
-);
+const Home = ({ navigation }) => {
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const getUserId = async () => {
+      try {
+        const storedUserId = await AsyncStorage.getItem("userId");
+        if (storedUserId !== null) {
+          setUserId(storedUserId);
+        } else {
+          console.log("UserId not found");
+        }
+      } catch (error) {
+        console.error("Error retrieving userId from AsyncStorage", error);
+      }
+    };
+
+    getUserId();
+  }, []);
+
+  return (
+    <View>
+      <Text>User Home</Text>
+      {userId ? (
+        <Text>User ID: {userId}</Text>
+      ) : (
+        <Text>Loading User ID...</Text>
+      )}
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+};
 
 export default Home;
