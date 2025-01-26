2dca9129c8148d51b0e6678044a726ca8b68bd59
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-01-26 22:27:24 +0100

    feat: load custom fonts for global use (#15)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AppNavigator from "./navigation/AppNavigator";
+import GlobalFont from "react-native-global-font";
 
 import LogoSmallText from "./components/logo/LogoSmallText";
 
@@ -10,8 +11,33 @@ const App = () => {
 
   useEffect(() => {
     const loadResources = () => {
-      // Load resources here
-      console.log("No resources to load yet");
+      // Load fonts and apply globally using the font names defined in the CSS (font-family)
+
+      // Nohemi Fonts
+      GlobalFont.applyGlobal("Nohemi-Regular"); // Regular weight
+      GlobalFont.applyGlobal("Nohemi-Black"); // Black weight
+      GlobalFont.applyGlobal("Nohemi-Bold"); // Bold weight
+      GlobalFont.applyGlobal("Nohemi-ExtraBold"); // ExtraBold weight
+      GlobalFont.applyGlobal("Nohemi-Light"); // Light weight
+      GlobalFont.applyGlobal("Nohemi-Thin"); // Thin weight
+      GlobalFont.applyGlobal("Nohemi-SemiBold"); // SemiBold weight
+      GlobalFont.applyGlobal("Nohemi-Medium"); // Medium weight
+      GlobalFont.applyGlobal("Nohemi-ExtraLight"); // ExtraLight weight
+
+      // Nunito Fonts
+      GlobalFont.applyGlobal("Nunito-Regular"); // Regular weight
+      GlobalFont.applyGlobal("Nunito-SemiBoldItalic"); // SemiBoldItalic weight
+      GlobalFont.applyGlobal("Nunito-BoldItalic"); // BoldItalic weight
+      GlobalFont.applyGlobal("Nunito-ExtraBold"); // ExtraBold weight
+      GlobalFont.applyGlobal("Nunito-SemiBold"); // SemiBold weight
+      GlobalFont.applyGlobal("Nunito-Bold"); // Bold weight
+      GlobalFont.applyGlobal("Nunito-MediumItalic"); // MediumItalic weight
+      GlobalFont.applyGlobal("Nunito-ExtraBoldItalic"); // ExtraBoldItalic weight
+      GlobalFont.applyGlobal("Nunito-Medium"); // Medium weight
+      GlobalFont.applyGlobal("Nunito-Italic"); // Italic weight
+
+      // Any other resources can be loaded here as needed
+      console.log("Fonts loaded");
     };
 
     setTimeout(() => {
