9d8e456a621526cb708dd73560ca2ecffaf12025
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-01-28 03:28:08 +0100

    chore: add svg icons

---
+++ src/components/icons/MagnifyingGlassIcon.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Svg, { Path } from "react-native-svg";
+
+const MagnifyingGlassIcon = ({
+  width = 24,
+  height = 24,
+  stroke = "#B1B0AF",
+  strokeWidth = "1.5",
+}) => (
+  <Svg
+    width={width}
+    height={height}
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <Path
+      d="M17.5 17.5L13.1694 13.1694M13.1694 13.1694C14.3004 12.0384 15 10.4759 15 8.75C15 5.29822 12.2018 2.5 8.75 2.5C5.29822 2.5 2.5 5.29822 2.5 8.75C2.5 12.2018 5.29822 15 8.75 15C10.4759 15 12.0384 14.3004 13.1694 13.1694Z"
+      stroke={stroke}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Svg>
+);
+
+export default MagnifyingGlassIcon;
