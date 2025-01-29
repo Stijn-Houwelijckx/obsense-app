95eaf7d07835214659547ffd293db5d75dee29fc
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-01-29 04:45:46 +0100

    style: style the landing page

--- src/screens/Auth/LandingPage.js
+++ src/screens/Auth/LandingPage.js
@@ -1,32 +1,92 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, Image } from "react-native";
+
+// Import Styles
+import { COLORS } from "../../styles/theme";
+import { globalStyles } from "../../styles/global";
+
+// Import Components
+import LogoSmallText from "../../components/logo/LogoSmallText";
+import CustomButton from "../../components/UI/CustomButton";
 
 const LandingPage = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Obsense</Text>
-      <Button
-        title="Login"
-        onPress={() => navigation.navigate("Login")} // Navigate to Login screen
+    <View style={[globalStyles.container, styles.container]}>
+      <Image
+        source={require("../../../assets/images/BallImage.png")}
+        style={[styles.ballImage, { top: 0, width: 245, height: 245 }]}
       />
-      <Button
-        title="Sign Up"
-        onPress={() => navigation.navigate("SignUp")} // Navigate to Signup screen
+      <Image
+        source={require("../../../assets/images/BallImage.png")}
+        style={[
+          styles.ballImage,
+          { top: 150, left: "10%", width: 105, height: 105 },
+        ]}
       />
+      <Image
+        source={require("../../../assets/images/BallImage.png")}
+        style={[
+          styles.ballImage,
+          { top: 150, right: "10%", width: 63, height: 63 },
+        ]}
+      />
+
+      <View
+        style={[globalStyles.secondaryContainer, styles.secondaryContainer]}
+      >
+        <View style={styles.headingContainer}>
+          <LogoSmallText />
+          <Text style={[styles.title, globalStyles.bodyLargeBold]}>
+            Where Reality and Imagination Collide
+          </Text>
+        </View>
+        <View style={styles.btnContainer}>
+          <CustomButton
+            variant="filled"
+            size="large"
+            title="Login"
+            onPress={() => navigation.navigate("Login")} // Navigate to Login screen
+            style={styles.btn}
+          />
+          <CustomButton
+            variant="outlined"
+            size="large"
+            title="Sign Up"
+            onPress={() => navigation.navigate("SignUp")} // Navigate to Signup screen
+            style={styles.btn}
+          />
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-end",
+  },
+  ballImage: {
+    position: "absolute",
+  },
+  secondaryContainer: {
+    borderRadius: 16,
+    width: "100%",
+    gap: 40,
+  },
+  headingContainer: {
     alignItems: "center",
-    padding: 20,
   },
   title: {
-    fontSize: 24,
-    marginBottom: 40,
+    color: COLORS.neutral[50],
+    textAlign: "center",
+    width: 164,
+  },
+  btnContainer: {
+    width: "100%",
+    gap: 12,
+  },
+  btn: {
+    width: "100%",
   },
 });
 
