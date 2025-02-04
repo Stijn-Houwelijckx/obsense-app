f097f96fcae65d55e81012ad104e31abb15539d2
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-02-04 04:08:48 +0100

    feat: add api /me endpoint to get current user

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,9 +1,15 @@
 const API_BASE_URL = "https://obsense-api.onrender.com/api/v1";
 
 const API_PATHS = {
+  // Auth Endpoints
   SIGNUP: `${API_BASE_URL}/users/signup`,
   LOGIN: `${API_BASE_URL}/users/login`,
-  // Add more endpoints here as needed
+
+  // Current User Endpoints
+  // GET /users/me -> Get current user
+  ME: `${API_BASE_URL}/users/me`,
+
+  // User Endpoints
 };
 
 export default API_PATHS;
