d2537f5d3582906578ddeff8f2b02a3c27a2f28f
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-02-07 02:14:00 +0100

    feat: add function to fetch collections for current artist from api (#22)

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import API_PATHS from "../config/apiConfig";
 
+// Function to get the current user from the API
 const getCurrentUser = async () => {
   try {
     const token = await AsyncStorage.getItem("userToken");
@@ -31,4 +32,44 @@ const getCurrentUser = async () => {
   }
 };
 
-export { getCurrentUser };
+const getCollectionsForCurrentArtist = async () => {
+  try {
+    const token = await AsyncStorage.getItem("userToken");
+    if (!token) {
+      return { status: "fail", message: "Unauthorized" }; // No token, unauthorized
+    }
+
+    const response = await axios.get(API_PATHS.ARTIST_COLLECTIONS, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      validateStatus: (status) => status >= 200 && status < 500, // Accept any 2xx or 4xx status as valid
+    });
+
+    // Handle success response
+    if (response.status === 200) {
+      return { status: "success", data: response.data };
+    }
+
+    // Handle 404 error
+    if (response.status === 404) {
+      return {
+        status: "fail",
+        data: {
+          code: 404,
+          message: "No collections found",
+        },
+      };
+    }
+
+    // Handle other errors
+    return {
+      status: "fail",
+      message: response.data?.data?.message || "Something went wrong",
+    };
+  } catch (error) {
+    return { status: "fail", message: error.message };
+  }
+};
+
+export { getCurrentUser, getCollectionsForCurrentArtist };
