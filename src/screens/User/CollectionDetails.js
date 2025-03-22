a9295aebc2b4ad69acb2b73e7c31f9e247d36982
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-03-22 22:30:06 +0100

    feat: add CollectionDetails page (#73)

---
+++ src/screens/User/CollectionDetails.js
@@ -0,0 +1,193 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import FastImage from "react-native-fast-image";
+
+// Import Utils
+import { getCollectionDetails } from "../../utils/api";
+
+// Import Styles
+import { globalStyles } from "../../styles/global";
+import { COLORS } from "../../styles/theme";
+
+// Import Icons
+import LocationPinIcon from "../../components/icons/LocationPinIcon";
+import BoxDashIcon from "../../components/icons/BoxDashIcon";
+import EyeIcon from "../../components/icons/EyeIcon";
+import HearthIcon from "../../components/icons/HearthIcon";
+
+// Import Components
+import DescriptionTextBox from "../../components/UI/DescriptionTextBox";
+import Badge from "../../components/UI/Badge";
+import CustomButton from "../../components/UI/CustomButton";
+
+const CollectionDetails = ({ navigation, route }) => {
+  const { collectionId } = route.params;
+  const [collectionDetailsData, setCollectionDetailsData] = useState([]); // State to store collection data
+  const [isLoading, setIsLoading] = useState(true); // State to manage loading state
+
+  useEffect(() => {
+    const getCollectionDataById = async () => {
+      const result = await getCollectionDetails(collectionId);
+
+      if (result.status === "success") {
+        // setUser(result.data.data.user); // Set user data
+        setCollectionDetailsData(result.data.collection); // Set collection data
+        console.log(result.data.collection); // Log collection data
+      } else {
+        console.log("Error getting user data:", result.message); // Log error message
+      }
+
+      setIsLoading(false); // Set loading state to false
+    };
+
+    getCollectionDataById(); // Call the function
+  }, []);
+
+  useEffect(() => {
+    if (collectionDetailsData?.title) {
+      navigation.setParams({ title: collectionDetailsData.title });
+    }
+  }, [collectionDetailsData, navigation]);
+
+  if (isLoading) {
+    return (
+      <View style={globalStyles.container}>
+        <ActivityIndicator size="large" color={COLORS.primary[500]} />
+      </View>
+    );
+  }
+
+  return (
+    <View style={[globalStyles.container, styles.container]}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.section}>
+          <View style={styles.tourInfo}>
+            <View style={styles.rowContainer}>
+              <LocationPinIcon size={20} stroke={COLORS.neutral[50]} />
+              <Text style={[globalStyles.bodyMediumRegular, styles.text]}>
+                {collectionDetailsData.city}
+              </Text>
+            </View>
+            <View style={styles.rowContainer}>
+              <BoxDashIcon size={20} stroke={COLORS.neutral[50]} />
+              <Text style={[globalStyles.bodyMediumRegular, styles.text]}>
+                {collectionDetailsData.objects}{" "}
+                {collectionDetailsData.objects === 1 ? "Artwork" : "Artworks"}
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.imageContainer}>
+            <Badge
+              size="large"
+              shape="rounded"
+              type="primary"
+              styleType="filled"
+              text={collectionDetailsData.type}
+              style={styles.badgeType}
+            />
+            <FastImage
+              style={styles.image}
+              source={{ uri: collectionDetailsData.coverImage.filePath }}
+            />
+            <Badge
+              size="large"
+              shape="rounded"
+              type="primary"
+              styleType="filled"
+              text={`Price: ${collectionDetailsData.price} tokens`}
+              style={styles.badgePrice}
+            />
+          </View>
+
+          <View style={styles.tourStats}>
+            <View style={styles.rowContainer}>
+              <EyeIcon size={20} stroke={COLORS.neutral[50]} />
+              <Text style={[globalStyles.bodyMediumRegular, styles.text]}>
+                {collectionDetailsData.views}
+              </Text>
+            </View>
+            <View style={styles.rowContainer}>
+              <HearthIcon size={20} stroke={COLORS.neutral[50]} />
+              <Text style={[globalStyles.bodyMediumRegular, styles.text]}>
+                {collectionDetailsData.likes}
+              </Text>
+            </View>
+          </View>
+          <DescriptionTextBox description={collectionDetailsData.description} />
+          <CustomButton
+            variant="filled"
+            size="large"
+            title={`Buy ${collectionDetailsData.type} - ${collectionDetailsData.price} tokens`}
+            onPress={() => console.log("Buy button pressed")}
+            style={{ width: "100%" }}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    gap: 32,
+  },
+  section: {
+    width: "100%",
+    marginTop: 4,
+    gap: 8,
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
+  imageContainer: {
+    position: "relative",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    borderRadius: 8,
+  },
+  badgeType: {
+    position: "absolute",
+    zIndex: 1,
+    top: 16,
+    left: 16,
+  },
+  badgePrice: {
+    position: "absolute",
+    zIndex: 1,
+    bottom: 16,
+    left: 16,
+  },
+  tourInfo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    gap: 20,
+  },
+  tourStats: {
+    flexDirection: "row",
+    gap: 20,
+  },
+  rowContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  text: {
+    color: COLORS.neutral[50],
+  },
+});
+
+export default CollectionDetails;
