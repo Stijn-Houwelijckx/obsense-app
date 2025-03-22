0f5e6540525225426626a0719baf680b767c13d5
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-03-22 21:34:28 +0100

    feat: add api fetch to get collection details

--- src/utils/api.js
+++ src/utils/api.js
@@ -115,7 +115,7 @@ const getCollectionsForCurrentArtist = async () => {
   }
 };
 
-// Function to get the collection details from the API
+// Function to get the artist collection details from the API
 const getArtistCollectionDetails = async (collectionId) => {
   try {
     const token = await AsyncStorage.getItem("userToken");
@@ -125,6 +125,39 @@ const getArtistCollectionDetails = async (collectionId) => {
 
     const response = await axios.get(
       `${API_PATHS.ARTIST_COLLECTIONS}/${collectionId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        validateStatus: (status) => status >= 200 && status < 500, // Accept any 2xx or 4xx status as valid
+      }
+    );
+
+    // Handle success response
+    if (response.status === 200) {
+      return { status: "success", data: response.data.data };
+    }
+
+    // Handle other errors
+    return {
+      status: "fail",
+      message: response.data?.data?.message || "Something went wrong",
+    };
+  } catch (error) {
+    return { status: "fail", message: error.message };
+  }
+};
+
+// Function to get the collection details from the API
+const getCollectionDetails = async (collectionId) => {
+  try {
+    const token = await AsyncStorage.getItem("userToken");
+    if (!token) {
+      return { status: "fail", message: "Unauthorized" }; // No token, unauthorized
+    }
+
+    const response = await axios.get(
+      `${API_PATHS.COLLECTIONS}/${collectionId}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -236,6 +269,7 @@ export {
   getArtists,
   getCollectionsForCurrentArtist,
   getArtistCollectionDetails,
+  getCollectionDetails,
   getOwnedCollections,
   getCollections,
 };
