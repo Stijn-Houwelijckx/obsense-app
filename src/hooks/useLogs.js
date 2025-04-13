4564fa0b054bb640a369c2730a4e2f812bf128ff
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-04-13 23:07:09 +0200

    feat: add useLogs hook to save logs and copy them to clipboard (#78)

---
+++ src/hooks/useLogs.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const useLogs = () => {
+  const [logs, setLogs] = useState([]);
+
+  const addLog = (message) => {
+    setLogs((prevLogs) => [...prevLogs, message]);
+  };
+
+  const clearLogs = () => {
+    setLogs([]);
+  };
+
+  const copyLogsToClipboard = () => {
+    const logsText = logs.join("\n");
+    Clipboard.setString(logsText);
+    Alert.alert("Logs Copied", "The logs have been copied to your clipboard.");
+  };
+
+  return { logs, addLog, clearLogs, copyLogsToClipboard };
+};
+
+export default useLogs;
