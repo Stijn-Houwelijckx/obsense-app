0c455b02d0f7eb28263ce995c46ba8557a8c5c67
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-04-18 18:35:03 +0200

    feat: add useLocation hook to continuously track location (#78)

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import Geolocation from "@react-native-community/geolocation";
+
+const useLocation = () => {
+  const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Watch location updates
+    const watchId = Geolocation.watchPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLocation({ latitude, longitude });
+      },
+      (error) => {
+        console.error("Error watching location: ", error);
+        setError(error.message);
+      },
+      { enableHighAccuracy: true, distanceFilter: 1 }
+    );
+
+    // Cleanup the watcher on unmount
+    return () => {
+      Geolocation.clearWatch(watchId);
+    };
+  }, []);
+
+  return { location, error };
+};
+
+export default useLocation;
