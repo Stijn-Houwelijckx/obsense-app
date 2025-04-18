94744a69ff8398463cedc9461a09df49b070323b
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-04-18 18:15:04 +0200

    feat: add location utils function (#78)

---
+++ src/utils/locationUtils.js
@@ -0,0 +1,19 @@
+import Geolocation from "@react-native-community/geolocation";
+
+const getCurrentLocation = () => {
+  return new Promise((resolve, reject) => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        resolve({ latitude, longitude });
+      },
+      (error) => {
+        console.error("Error getting current location: ", error);
+        reject(error);
+      },
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  });
+};
+
+export { getCurrentLocation };
