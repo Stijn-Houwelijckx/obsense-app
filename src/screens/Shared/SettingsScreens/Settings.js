67306911d019f3f872b80c8dc7d9b84f592fe062
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-04-20 18:32:35 +0200

    feat: create and style the settings page (#80)

--- src/screens/Shared/SettingsScreens/Settings.js
+++ src/screens/Shared/SettingsScreens/Settings.js
@@ -1,10 +1,57 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  Button,
+  ActivityIndicator,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useActiveCollection } from "../../../context/ActiveCollectionContext";
+import FastImage from "react-native-fast-image";
+
+// Import Utils
+import { getCurrentUser } from "../../../utils/api";
+
+// Import Styles
+import { globalStyles } from "../../../styles/global";
+import { COLORS } from "../../../styles/theme";
+
+// Import Icons
+import UserCircleIcon from "../../../components/icons/UserCircleIcon";
+import EuroCircleIcon from "../../../components/icons/EuroCircleIcon";
+import KeyIcon from "../../../components/icons/KeyIcon";
+import BellIcon from "../../../components/icons/BellIcon";
+import LockClosedIcon from "../../../components/icons/LockClosedIcon";
+import ShieldCheckIcon from "../../../components/icons/ShieldCheckIcon";
+import LogoutIcon from "../../../components/icons/LogoutIcon";
+
+// Custom Components
+import NavList from "../../../components/UI/NavList";
+import NavListItem from "../../../components/UI/NavListItem";
+import CustomButton from "../../../components/UI/CustomButton";
 
 const Settings = ({ navigation, handleAuthChangeSuccess }) => {
+  const [user, setUser] = useState(null); // State to store user data
+  const [isLoading, setIsLoading] = useState(true); // State to manage loading state
   const { clearActiveCollection } = useActiveCollection();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const result = await getCurrentUser();
+
+      if (result.status === "success") {
+        setUser(result.data.user); // Set user data
+      } else {
+        console.log("Error getting user data:", result.message); // Log error message
+      }
+
+      setIsLoading(false); // Set loading state to false
+    };
+
+    getUser(); // Call the function
+  }, []);
 
   const handleLogout = async () => {
     try {
@@ -25,28 +72,103 @@ const Settings = ({ navigation, handleAuthChangeSuccess }) => {
     }
   };
 
+  if (isLoading) {
+    return (
+      <View style={globalStyles.container}>
+        <ActivityIndicator size="large" color={COLORS.primary[500]} />
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Settings</Text>
-      <Button
-        title="Go to Account Settings"
-        onPress={() => navigation.navigate("AccountSettings")}
-      />
+    <View style={[globalStyles.container, styles.container]}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.profileHeader}>
+          <FastImage
+            source={{
+              uri: user.profilePicture.filePath,
+            }}
+            style={styles.profilePicture}
+          />
+          <Text style={[globalStyles.headingH6Medium, styles.profileName]}>
+            {user.firstName} {user.lastName}
+          </Text>
+          <Text style={[globalStyles.bodyMediumRegular, styles.profileEmail]}>
+            {user.email}
+          </Text>
+        </View>
 
-      <Button
-        title="Go to Change Password"
-        onPress={() => navigation.navigate("ChangePassword")}
-      />
+        <NavList>
+          <NavListItem
+            title="Account Settings"
+            icon={UserCircleIcon}
+            onPress={() => navigation.navigate("AccountSettings")}
+          />
+          <NavListItem
+            title="My Tokens"
+            icon={EuroCircleIcon}
+            onPress={() => console.log("My Tokens")}
+          />
+          <NavListItem
+            title="Change Password"
+            icon={KeyIcon}
+            onPress={() => navigation.navigate("ChangePassword")}
+          />
+          <NavListItem
+            title="Notifications"
+            icon={BellIcon}
+            onPress={() => navigation.navigate("Notification")}
+          />
+          <NavListItem
+            title="Privacy & Cookies"
+            icon={LockClosedIcon}
+            onPress={() => console.log("Privacy & Cookies")}
+          />
+          <NavListItem
+            title="Terms and Conditions"
+            icon={ShieldCheckIcon}
+            onPress={() => console.log("Security & Login")}
+          />
+        </NavList>
 
-      <Button
-        title="Go to Notifications"
-        onPress={() => navigation.navigate("Notification")}
-      />
-
-      {/* Logout Button */}
-      <Button title="Logout" onPress={handleLogout} />
+        <CustomButton
+          variant="text"
+          size="large"
+          title="Logout"
+          leadingIcon={LogoutIcon}
+          onPress={handleLogout}
+          style={styles.logoutButton}
+        />
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 20,
+    justifyContent: "",
+    alignItems: "",
+  },
+  profileHeader: {
+    gap: 8,
+    alignItems: "center",
+  },
+  profilePicture: {
+    width: 130,
+    height: 130,
+    borderRadius: 9999,
+    marginBottom: 8,
+  },
+  profileName: {
+    color: COLORS.neutral[50],
+  },
+  profileEmail: {
+    color: COLORS.neutral[100],
+  },
+  logoutButton: {
+    justifyContent: "flex-start",
+  },
+});
 
 export default Settings;
