b66ce9ce0a72ff7167b7011b29aba12a13697dc4
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-04-21 03:10:36 +0200

    feat: change password (#45)

--- src/screens/Shared/SettingsScreens/ChangePassword.js
+++ src/screens/Shared/SettingsScreens/ChangePassword.js
@@ -1,10 +1,186 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 
-const ChangePassword = ({ navigation }) => (
-  <View>
-    <Text>Change Password</Text>
-  </View>
-);
+// Import Utils
+import { changeCurrentUserPassword } from "../../../utils/api";
+
+// Import Styles
+import { globalStyles } from "../../../styles/global";
+import { COLORS } from "../../../styles/theme";
+
+// Import Icons
+import LockClosedIcon from "../../../components/icons/LockClosedIcon";
+import EyeIcon from "../../../components/icons/EyeIcon";
+import EyeSlashIcon from "../../../components/icons/EyeSlashIcon";
+
+// Custom Components
+import InputField from "../../../components/UI/InputField";
+import CustomButton from "../../../components/UI/CustomButton";
+
+const ChangePassword = ({ navigation }) => {
+  const [error, setError] = useState(""); // State to manage error
+  const [message, setMessage] = useState(""); // State to manage message
+
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handlePasswordChange = async () => {
+    setError(""); // Reset error state
+    setMessage(""); // Reset message state
+
+    // Function to handle password change
+    if (newPassword !== confirmPassword) {
+      setError("Password doesn't match, try again.");
+      return;
+    }
+
+    const result = await changeCurrentUserPassword(
+      currentPassword,
+      newPassword
+    );
+
+    if (result.status === "success") {
+      setMessage("Password changed successfully.");
+
+      setCurrentPassword("");
+      setNewPassword("");
+      setConfirmPassword("");
+    } else {
+      setError(result.message);
+    }
+
+    setTimeout(() => {
+      setMessage("");
+    }, 2000);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={[globalStyles.container, styles.container]}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <View style={styles.formContainer}>
+            <View style={styles.fieldsContainer}>
+              <InputField
+                type="password"
+                label="Current Password"
+                leadingIcon={
+                  <LockClosedIcon size={20} stroke={COLORS.neutral[500]} />
+                }
+                placeholder="Current Password"
+                secureTextEntry={false}
+                value={currentPassword}
+                onChangeText={setCurrentPassword}
+                trailingIcon={{
+                  visible: (
+                    <EyeSlashIcon size={20} stroke={COLORS.neutral[500]} />
+                  ),
+                  hidden: <EyeIcon size={20} stroke={COLORS.neutral[500]} />,
+                }}
+                autoCapitalize="none"
+              />
+            </View>
+            <View style={styles.fieldsContainer}>
+              <InputField
+                type="password"
+                label="New Password"
+                leadingIcon={
+                  <LockClosedIcon size={20} stroke={COLORS.neutral[500]} />
+                }
+                placeholder="New Password"
+                secureTextEntry={false}
+                value={newPassword}
+                onChangeText={setNewPassword}
+                trailingIcon={{
+                  visible: (
+                    <EyeSlashIcon size={20} stroke={COLORS.neutral[500]} />
+                  ),
+                  hidden: <EyeIcon size={20} stroke={COLORS.neutral[500]} />,
+                }}
+                autoCapitalize="none"
+              />
+            </View>
+            <View style={styles.fieldsContainer}>
+              <InputField
+                type="password"
+                label="Confirm New Password"
+                leadingIcon={
+                  <LockClosedIcon size={20} stroke={COLORS.neutral[500]} />
+                }
+                placeholder="Confirm New Password"
+                secureTextEntry={false}
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                trailingIcon={{
+                  visible: (
+                    <EyeSlashIcon size={20} stroke={COLORS.neutral[500]} />
+                  ),
+                  hidden: <EyeIcon size={20} stroke={COLORS.neutral[500]} />,
+                }}
+                autoCapitalize="none"
+              />
+            </View>
+            {message && (
+              <Text
+                style={[
+                  globalStyles.bodyMediumRegular,
+                  { color: COLORS.success[500], marginTop: 12 },
+                ]}
+              >
+                {message}
+              </Text>
+            )}
+            {error && (
+              <Text
+                style={[
+                  globalStyles.bodyMediumRegular,
+                  { color: COLORS.error[500], marginTop: 12 },
+                ]}
+              >
+                {error}
+              </Text>
+            )}
+            <CustomButton
+              variant="filled"
+              size="large"
+              title="Save Changes"
+              onPress={() => handlePasswordChange()}
+              style={{ marginTop: 12 }}
+            />
+          </View>
+        </ScrollView>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 20,
+    justifyContent: "",
+    alignItems: "",
+  },
+  formContainer: {
+    gap: 20,
+    backgroundColor: COLORS.primaryNeutral[800],
+    padding: 16,
+    borderRadius: 16,
+  },
+  fieldsContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  inputField: {
+    flex: 1,
+  },
+});
 
 export default ChangePassword;
