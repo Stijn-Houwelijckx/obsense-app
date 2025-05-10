85beb5104fc211e270ba675d8aa1d4e7ddecc4ca
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-05-10 19:13:27 +0200

    feat: add api function to delete placed object

--- src/utils/api.js
+++ src/utils/api.js
@@ -566,6 +566,50 @@ const getPlacedObjectsByCollection = async (collectionId) => {
   }
 };
 
+// Delete a placed object
+const deletePlacedObject = async (placedObjectId) => {
+  try {
+    const token = await AsyncStorage.getItem("userToken");
+    if (!token) {
+      return { status: "fail", message: "Unauthorized" }; // No token, unauthorized
+    }
+
+    const response = await axios.delete(
+      `${API_PATHS.PLACED_OBJECTS}/${placedObjectId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        validateStatus: (status) => status >= 200 && status < 500, // Accept any 2xx or 4xx status as valid
+      }
+    );
+
+    // Handle success response
+    if (response.status === 200) {
+      return { status: "success", data: response.data.data };
+    }
+
+    // Handle 404 error
+    if (response.status === 404) {
+      return {
+        status: "fail",
+        data: {
+          code: 404,
+          message: "Placed object not found",
+        },
+      };
+    }
+
+    // Handle other errors
+    return {
+      status: "fail",
+      message: response.data?.data?.message || "Something went wrong",
+    };
+  } catch (error) {
+    return { status: "fail", message: error.message };
+  }
+};
+
 // Updat tokens for the current user from the API
 const updateTokens = async (tokenAmount) => {
   try {
@@ -618,5 +662,6 @@ export {
   getCollections,
   savePlacedObject,
   getPlacedObjectsByCollection,
+  deletePlacedObject,
   updateTokens,
 };
