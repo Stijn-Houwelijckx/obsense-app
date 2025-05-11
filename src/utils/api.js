e217ab12612929dd23100877c33d50ccc78700c9
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-05-11 03:18:29 +0200

    feat: add function to get all genres from api

--- src/utils/api.js
+++ src/utils/api.js
@@ -646,6 +646,36 @@ const updateTokens = async (tokenAmount) => {
   }
 };
 
+// Get all genres from the API
+const getGenres = async () => {
+  try {
+    const token = await AsyncStorage.getItem("userToken");
+    if (!token) {
+      return { status: "fail", message: "Unauthorized" }; // No token, unauthorized
+    }
+
+    const response = await axios.get(API_PATHS.GENRES, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      validateStatus: (status) => status >= 200 && status < 500, // Accept any 2xx or 4xx status as valid
+    });
+
+    // Handle success response
+    if (response.status === 200) {
+      return { status: "success", data: response.data.data };
+    }
+
+    // Handle other errors
+    return {
+      status: "fail",
+      message: response.data?.data?.message || "Something went wrong",
+    };
+  } catch (error) {
+    return { status: "fail", message: error.message };
+  }
+};
+
 export {
   changeCurrentUserPassword,
   getCurrentUser,
@@ -664,4 +694,5 @@ export {
   getPlacedObjectsByCollection,
   deletePlacedObject,
   updateTokens,
+  getGenres,
 };
