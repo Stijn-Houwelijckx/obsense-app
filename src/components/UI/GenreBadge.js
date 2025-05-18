39dacecc7ce5b0a2b230a1e6f758a57b296d841b
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-05-18 02:59:38 +0200

    feat: add GenreBadge component

---
+++ src/components/UI/GenreBadge.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+// Import Styles
+import { globalStyles } from "../../styles/global";
+import { COLORS } from "../../styles/theme";
+
+const GenreItem = ({ title, style }) => {
+  return (
+    <View style={[styles.container, style]}>
+      <Text style={[globalStyles.labelSmallBold, styles.title]}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.primaryNeutral[600],
+    paddingHorizontal: 20,
+    paddingVertical: 8,
+    borderRadius: 8,
+    alignSelf: "flex-start",
+  },
+  title: {
+    color: COLORS.neutral[50],
+  },
+});
+
+export default GenreItem;
