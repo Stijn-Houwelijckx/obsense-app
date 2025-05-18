443cacd5063051c240e39054d1ae5cec2c270e43
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-05-18 02:44:16 +0200

    feat: add api function to get all collections by genreId

--- src/utils/api.js
+++ src/utils/api.js
@@ -487,6 +487,51 @@ const getCollections = async (page = 1, limit = 20) => {
   }
 };
 
+// Function to get all collections by genre from the API
+const getCollectionsByGenre = async (genreId, page = 1, limit = 20) => {
+  try {
+    const token = await AsyncStorage.getItem("userToken");
+    if (!token) {
+      return { status: "fail", message: "Unauthorized" }; // No token, unauthorized
+    }
+    const response = await axios.get(
+      `${API_PATHS.COLLECTIONS}/genre/${genreId}?page=${page}&limit=${limit}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        validateStatus: (status) => status >= 200 && status < 500, // Accept any 2xx or 4xx status as valid
+      }
+    );
+
+    // Handle success response
+    if (response.status === 200) {
+      return { status: "success", data: response.data.data };
+    }
+
+    // Handle 204 response
+    if (response.status === 204) {
+      return {
+        status: "success",
+        data: {
+          code: 204,
+          message: "No collections found",
+          collections: [],
+          hasMore: false,
+        },
+      };
+    }
+
+    // Handle other errors
+    return {
+      status: "fail",
+      message: response.data?.data?.message || "Something went wrong",
+    };
+  } catch (error) {
+    return { status: "fail", message: error.message };
+  }
+};
+
 // Save placed object to the API
 const savePlacedObject = async (placedObject) => {
   try {
@@ -690,6 +735,7 @@ export {
   getOwnedCollections,
   purchaseCollection,
   getCollections,
+  getCollectionsByGenre,
   savePlacedObject,
   getPlacedObjectsByCollection,
   deletePlacedObject,
