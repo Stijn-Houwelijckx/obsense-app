cb1641af68a06d71bf19ba5d9aa090aa0a32854b
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-05-20 00:33:26 +0200

    feat: add export file for UI components to simplify imports (#89)

---
+++ src/components/UI/index.js
@@ -0,0 +1,47 @@
+import ArtistCard from "./ArtistCard";
+import ArtistCollectionCard from "./ArtistCollectionCard";
+import ArtistItem from "./ArtistItem";
+import Badge from "./Badge";
+import BoughtCollectionCard from "./BoughtCollectionCard";
+import CollectionCard from "./CollectionCard";
+import CollectionListItem from "./CollectionListItem";
+import CustomButton from "./CustomButton";
+import DescriptionTextBox from "./DescriptionTextBox";
+import GenreBadge from "./GenreBadge";
+import GenreItem from "./GenreItem";
+import Header from "./Header";
+import IconButton from "./IconButton";
+import InputField from "./InputField";
+import NavList from "./NavList";
+import NavListItem from "./NavListItem";
+import NumberSelector from "./NumberSelector";
+import ObjectCard from "./ObjectCard";
+import ProgressIndicator from "./ProgressIndicator";
+import SearchInput from "./SearchInput";
+import SocialButton from "./SocialButton";
+import WalletCard from "./WalletCard";
+
+export {
+  ArtistCard,
+  ArtistCollectionCard,
+  ArtistItem,
+  Badge,
+  BoughtCollectionCard,
+  CollectionCard,
+  CollectionListItem,
+  CustomButton,
+  DescriptionTextBox,
+  GenreBadge,
+  GenreItem,
+  Header,
+  IconButton,
+  InputField,
+  NavList,
+  NavListItem,
+  NumberSelector,
+  ObjectCard,
+  ProgressIndicator,
+  SearchInput,
+  SocialButton,
+  WalletCard,
+};
