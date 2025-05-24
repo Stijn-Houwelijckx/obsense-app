97ed18811fbb041f13b0b3e4255b6978a93e2d8e
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-05-25 00:02:04 +0200

    feat: add red colors to theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -82,6 +82,21 @@ export const COLORS = {
     900: "#7f1d1d",
     950: "#450a0a",
   },
+
+  // red Colors
+  red: {
+    50: "#fef2f2",
+    100: "#fee2e2",
+    200: "#fecaca",
+    300: "#fca5a5",
+    400: "#f87171",
+    500: "#ef4444",
+    600: "#dc2626",
+    700: "#b91c1c",
+    800: "#991b1b",
+    900: "#7f1d1d",
+    950: "#450a0a",
+  },
 };
 
 export const FONT_SIZES = {
