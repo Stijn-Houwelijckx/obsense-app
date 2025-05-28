8289fcc7d1f0989aed43d0cf9a8e53dd8f33377f
Stijn Houwelijckx <stijn@houwelijckx.be>
2025-05-29 00:37:31 +0200

    feat: check if user has an internet connection, if not, show an overlay and don't render the rest of the app (#107)

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, View, Text, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import SystemNavigationBar from "react-native-system-navigation-bar";
+import NetInfo from "@react-native-community/netinfo";
+
 import ArtistNavigator from "./ArtistNavigator";
 import UserNavigator from "./UserNavigator";
 import AuthNavigator from "./AuthNavigator";
 import RoleSelection from "../screens/Auth/RoleSelection";
+
+import { COLORS } from "../styles/theme";
+import { globalStyles } from "../styles/global";
 
 import {
   checkLocationPermission,
@@ -16,13 +21,23 @@ import {
 // Import Contexts
 import ContextProvider from "../context/ContexProvider";
 
-import { COLORS } from "../styles/theme";
-
 const AppNavigator = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isArtist, setIsArtist] = useState(false);
   const [isUpdated, setIsUpdated] = useState(false); // Track if the status is updated
   const [selectedRole, setSelectedRole] = useState(null);
+
+  const [isOffline, setIsOffline] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsOffline(!state.isConnected);
+      console.log("Network status: ", state.isConnected ? "Online" : "Offline");
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   useEffect(() => {
     SystemNavigationBar.setNavigationColor(COLORS.primaryNeutral[800], "light");
@@ -74,8 +89,17 @@ const AppNavigator = () => {
           barStyle="light-content" // Makes the status bar text white
           backgroundColor={COLORS.primaryNeutral[900]} // Sets the background color of the status bar
         />
+        {/* Full Screen Offline Overlay */}
+        {isOffline && (
+          <View style={[styles.offlineOverlay]}>
+            <Text style={[styles.text, globalStyles.bodyLargeBold]}>
+              You are currently offline. Please check your internet connection.
+            </Text>
+          </View>
+        )}
+
         {/* If the user is not logged in, show AuthNavigator */}
-        {isLoggedIn ? (
+        {isLoggedIn && isOffline === false ? (
           isArtist ? (
             selectedRole === "artist" ? (
               <ArtistNavigator
@@ -100,5 +124,30 @@ const AppNavigator = () => {
     </ContextProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  offlineOverlay: {
+    flex: 1,
+    zIndex: 1000, // Ensure the overlay is on top
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    padding: 16,
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: COLORS.neutral[50],
+    textAlign: "center",
+    padding: 28,
+    backgroundColor: COLORS.primaryNeutral[800],
+    borderRadius: 10,
+  },
+});
 
 export default AppNavigator;
